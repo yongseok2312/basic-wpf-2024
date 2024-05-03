96c216e837947c34ea55e9ca9937ac0f6e6fca32
yongseok2312 <lyos2312@naver.com>
2024-05-03 09:01:13 +0900

    3일차 수정

--- day03/wpf04_mvvm_app/ex07_EmployeeMngApp/Helpers/Common.cs
+++ day03/wpf04_mvvm_app/ex07_EmployeeMngApp/Helpers/Common.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ex07_EmployeeMngApp.Helpers
+﻿namespace ex07_EmployeeMngApp.Helpers
 {
-    internal class Common
+    public class Common
     {
+        public static readonly string CONNSTRING = "Data Source=localhost;" +
+                                          "Initial Catalog=EMS;" +
+                                          "Persist Security Info=True;" +
+                                          "User ID=ems_user;Encrypt=False;" +
+                                          "Password=ems_p@ss";
     }
 }
--- day03/wpf04_mvvm_app/ex07_EmployeeMngApp/Models/Employees.cs
+++ day03/wpf04_mvvm_app/ex07_EmployeeMngApp/Models/Employees.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ex07_EmployeeMngApp.Models
+﻿namespace ex07_EmployeeMngApp.Models
 {
-    internal class Employees
+    public class Employees
     {
+        public int Id { get; set; }
+        public string EmpName { get; set; }
+        public string Salary { get; set; }
+        public int DeptName { get; set; }
+        public int Addr { get; set; }
+
+        public static readonly string SELECT_QUERY =  @"SELECT [Id]
+                                                              ,[EmpName]
+                                                              ,[Salary]
+                                                              ,[DeptName]
+                                                              ,[Addr]
+                                                          FROM [Employees]";
+        public static readonly string INSERT_QUERY = @"INSERT INTO [Employees]
+                                                                       ([EmpName]
+                                                                       ,[Salary]
+                                                                       ,[DeptName]
+                                                                       ,[Addr])
+                                                                 VALUES
+                                                                       (@EmpName
+                                                                       ,@Salary
+                                                                       ,@DeptName
+                                                                       ,@Addr)";
+        public static readonly string UPDATE_QUERY = @"UPDATE [Employees]
+                                                                   SET [EmpName] = @EmpName
+                                                                      ,[Salary] = @Salary
+                                                                      ,[DeptName] = @DeptName
+                                                                      ,[Addr] = @Addr
+                                                                 WHERE Id = @Id";
+        public static readonly string DELETE_QUERY = @"DELETE FROM [Employees]
+                                                                 WHERE Id=@Id";
     }
 }
